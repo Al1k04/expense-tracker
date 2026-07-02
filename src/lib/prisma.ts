d980e485984adebfe010d89bdd73ee8d0a8d07e9
Al1k04 <alikprog04@gmail.com>
2026-07-02 16:38:26 +0400

    fix: simplify prisma client config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,12 @@
 import { PrismaClient } from "@/generated/prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const prismaClientSingleton = () => new PrismaClient();
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
